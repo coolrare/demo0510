6d621f0d790935d3506f7d76fc52a6da2c1a2c42
Will保哥 <doggy.huang@gmail.com>
2017-05-24 09:41:40 +0800

    實作 OnInit 介面

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DataService } from 'app/data.service';
 
 @Component({
@@ -6,7 +6,7 @@ import { DataService } from 'app/data.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app works!';
   data:any;
 
