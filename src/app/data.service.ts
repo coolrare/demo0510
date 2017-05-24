cdaf9cd3b0aa0c8b50b312d1adca8e700ba16f71
Will保哥 <doggy.huang@gmail.com>
2017-05-24 11:18:17 +0800

    加入刪除資料的實際 API 呼叫 ( 使用 json-server 工具 )
    1. 將 /src/api/db.json 複製到專案以外的某個資料夾下 ( C:\Projects )
    2. 透過命令提示字元進入該資料夾 ( C:\Projects )
    3. 執行命令: json-server -w .\db.json
    4. 測試 API: http://localhost:3000/articles
    5. 修改 src/app/data.service.ts 程式

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -7,14 +7,24 @@ export class DataService {
   data: any;
 
   constructor(private http: Http) {
-    http.get('/api/articles.json')
+    http.get('http://localhost:3000/articles')
       .subscribe(res => {
         this.data = res.json();
       });
   }
 
   deleteArticle(item) {
-    const idx = this.data.indexOf(item);
-    this.data.splice(idx, 1);
+    // const idx = this.data.indexOf(item);
+    // this.data.splice(idx, 1);
+
+    this.http.delete('http://localhost:3000/articles/' + item.id)
+      .subscribe(res => {
+        if (res.status === 200) {
+          this.http.get('http://localhost:3000/articles')
+            .subscribe(re => {
+              this.data = re.json();
+            });
+        }
+      });
   }
 }
